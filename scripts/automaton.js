fc3f71d6e3e7d9ba4ea2599a4a4d50469d341951
Tubs <tubskleyson@gmail.com>
2019-06-05 19:28:02 -0300

    Modelo das classes principais

---
+++ scripts/automaton.js
@@ -0,0 +1,21 @@
+class Automaton{
+
+    constructor(estados, alfabeto, inicial, finais, trans){
+
+        /*
+            Definição da pilha
+            Estados iniciais
+        */
+
+    }
+
+    ler(palavra){
+
+        /*
+            Leitura símbolo a símbolo com atualização da pilha
+            ** pra cada linha do tempo possível
+        */
+
+    }
+
+}
---
+++ scripts/estado.js
@@ -0,0 +1,15 @@
+class Estado{
+
+    constructor(trans){
+
+        // gurada as transições da forma que for mais eficiente
+
+    }
+
+    ler(simbolo, pilha){
+
+        // lê o símbolo e retorna os estados possíveis junto com o futuro da pilha
+
+    }
+
+}
