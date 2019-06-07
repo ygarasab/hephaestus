1552fc5223b63bab892faaf137b71d43e6edfcc3
Tubs <tubskleyson@gmail.com>
2019-06-06 22:29:53 -0300

    documentação

--- scripts/automaton.js
+++ scripts/automaton.js
@@ -1,20 +1,29 @@
 class Automaton{
+    
+    /**
+     * Toma as caracterísiticas básicas do autômato e gera seus estados
+     * 
+     * @param {String[]} estados Lista de estados do autômato
+     * @param {String[]]} alfabeto Lista de símbolos que o autômato processa
+     * @param {String} inicial Estado inicial do autômato
+     * @param {String[]]} finais Estados finais do autômato
+     * @param {String[]]} trans Lista de transições possíveis
+     */
 
     constructor(estados, alfabeto, inicial, finais, trans){
-
-        /*
-            Definição da pilha
-            Estados iniciais
-        */
+        
 
     }
 
+    /**
+     * Recebe uma string e verifica de ela é processada ou não por este automato
+     * @param {String} palavra Palavra a ser processada
+     * @returns {Boolean}
+     */
+
     ler(palavra){
 
-        /*
-            Leitura símbolo a símbolo com atualização da pilha
-            ** pra cada linha do tempo possível
-        */
+        return resultado
 
     }
 
--- scripts/estado.js
+++ scripts/estado.js
@@ -1,14 +1,24 @@
 class Estado{
 
+    /**
+     * Organiza as transições e estabelece padrões pro estado
+     * @param {[String]} trans Conjunto de transições possíveis para esse autômato
+     */
+
     constructor(trans){
 
-        // gurada as transições da forma que for mais eficiente
-
+       
     }
 
+    /**
+     * Recebe um símbolo para processamento, junto com uma pilha, e retorna uma lista, onde 
+     * cada ítem contem um automato e sua pilha resultante para cada processamento possível
+     * @param {String} simbolo Símbolo a ser processado
+     * @param {[String]} pilha Pilha usada no processamento
+     * @returns {[String,[String]]}  
+     */
     ler(simbolo, pilha){
 
-        // lê o símbolo e retorna os estados possíveis junto com o futuro da pilha
 
     }
 
