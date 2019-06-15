32e693884d2eb9444006884d749a3fade398be81
Eduardo Cardoso <eduardo.gil.s.cardoso@gmail.com>
2019-06-15 10:34:55 -0300

    Correção da referência a automatos em vez de estados
    Boa Gabi

--- scripts/estado.js
+++ scripts/estado.js
@@ -2,7 +2,7 @@ class Estado{
 
     /**
      * Organiza as transições e estabelece padrões pro estado
-     * @param {[String]} trans Conjunto de transições possíveis para esse autômato
+     * @param {[String]} trans Conjunto de transições possíveis para esse estado
      */
 
     constructor(trans){
@@ -12,7 +12,7 @@ class Estado{
 
     /**
      * Recebe um símbolo para processamento, junto com uma pilha, e retorna uma lista, onde 
-     * cada ítem contem um automato e sua pilha resultante para cada processamento possível
+     * cada ítem contem um estado e sua pilha resultante para cada processamento possível
      * @param {String} simbolo Símbolo a ser processado
      * @param {[String]} pilha Pilha usada no processamento
      * @returns {[String,[String]]}  
